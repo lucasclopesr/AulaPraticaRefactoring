3fe650579b02adcdd723412e0ea0e100ee29d9f4
Lucas Lopes <lucasclopesr@gmail.com>
2021-03-12 14:45:46 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -3,10 +3,6 @@ public abstract class Price {
    public abstract double getCharge(int daysRented);
 
    public int getFrequentRenterPoints(int daysRented) {
-       // add bonus for a two day new release rental
-        if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
-            daysRented > 1) return 2;
-
-        return 1;
+       return 1;
    }
 }
